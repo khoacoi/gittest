d93b991d6c016f5d4fb580e81f758c8b69beaad2
huyptrankms <htran@lsscontract.com>
2023-02-16 15:45:12 +0700

    [Huy] add test 1

--- valungtung.js
+++ valungtung.js
@@ -1,6 +1,8 @@
 const isValungtungDay = () => {
   const date = new Date();
   console.log('isValungtungDay');
+  console.log('isValungtungDay');
+  console.log('isValungtungDay');
   return date.getDay() === 14 && date.getMonth() === 2;
 }
 
