5f38844e4153184f0082b317d04585970c2b6ec1
huyptrankms <htran@lsscontract.com>
2023-02-16 15:12:06 +0700

    [Huy] add function valentine day.

---
+++ valungtung.js
@@ -0,0 +1,7 @@
+const isValungtungDay = () => {
+  const date = new Date();
+  console.log('isValungtungDay');
+  return date.getDay() === 14 && date.getMonth() === 2;
+}
+
+export default isValungtungDay;
